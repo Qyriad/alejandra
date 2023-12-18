9f8e5f26703f35420287130438418735fe4ff7be
Qyriad <qyriad@qyriad.me>
2023-12-18 00:02:55 -0700

    allow function args to be on the same line as key =

--- src/alejandra/src/rules/key_value.rs
+++ src/alejandra/src/rules/key_value.rs
@@ -1,8 +1,10 @@
-pub(crate) fn rule(
-    build_ctx: &crate::builder::BuildCtx,
-    node: &rnix::SyntaxNode,
-) -> std::collections::LinkedList<crate::builder::Step> {
-    let mut steps = std::collections::LinkedList::new();
+use std::collections::LinkedList;
+
+use crate::builder::BuildCtx;
+use crate::builder::Step as BuildStep;
+
+pub(crate) fn rule(build_ctx: &BuildCtx, node: &rnix::SyntaxNode) -> LinkedList<BuildStep> {
+    let mut steps = LinkedList::new();
 
     let mut children = crate::children::Children::new(build_ctx, node);
 
@@ -11,9 +13,9 @@ pub(crate) fn rule(
     // a
     let child = children.get_next().unwrap();
     if vertical {
-        steps.push_back(crate::builder::Step::FormatWider(child));
+        steps.push_back(BuildStep::FormatWider(child));
     } else {
-        steps.push_back(crate::builder::Step::Format(child));
+        steps.push_back(BuildStep::Format(child));
     }
 
     // /**/
@@ -21,29 +23,27 @@ pub(crate) fn rule(
     children.drain_trivia(|element| match element {
         crate::children::Trivia::Comment(text) => {
             comment = true;
-            steps.push_back(crate::builder::Step::NewLine);
-            steps.push_back(crate::builder::Step::Pad);
-            steps.push_back(crate::builder::Step::Comment(text));
+            steps.push_back(BuildStep::NewLine);
+            steps.push_back(BuildStep::Pad);
+            steps.push_back(BuildStep::Comment(text));
         }
         crate::children::Trivia::Whitespace(_) => {}
     });
     if comment {
-        steps.push_back(crate::builder::Step::NewLine);
-        steps.push_back(crate::builder::Step::Pad);
+        steps.push_back(BuildStep::NewLine);
+        steps.push_back(BuildStep::Pad);
     } else {
-        steps.push_back(crate::builder::Step::Whitespace);
+        steps.push_back(BuildStep::Whitespace);
     }
 
     // peek: =
     let child_equal = children.get_next().unwrap();
 
     // peek: /**/
-    let mut comments_before = std::collections::LinkedList::new();
+    let mut comments_before = LinkedList::new();
     let mut newlines = false;
     children.drain_trivia(|element| match element {
-        crate::children::Trivia::Comment(text) => {
-            comments_before.push_back(crate::builder::Step::Comment(text))
-        }
+        crate::children::Trivia::Comment(text) => comments_before.push_back(BuildStep::Comment(text)),
         crate::children::Trivia::Whitespace(text) => {
             if crate::utils::count_newlines(&text) > 0 {
                 newlines = true;
@@ -72,82 +72,89 @@ pub(crate) fn rule(
     };
 
     // peek: /**/
-    let mut comments_after = std::collections::LinkedList::new();
+    let mut comments_after = LinkedList::new();
     children.drain_trivia(|element| match element {
-        crate::children::Trivia::Comment(text) => {
-            comments_after.push_back(crate::builder::Step::Comment(text))
-        }
+        crate::children::Trivia::Comment(text) => comments_after.push_back(BuildStep::Comment(text)),
         crate::children::Trivia::Whitespace(_) => {}
     });
 
     // =
     let mut dedent = false;
-    steps.push_back(crate::builder::Step::Format(child_equal));
+    steps.push_back(BuildStep::Format(child_equal));
 
     if vertical {
         if !comments_before.is_empty() || !comments_after.is_empty() {
             dedent = true;
-            steps.push_back(crate::builder::Step::Indent);
-            steps.push_back(crate::builder::Step::NewLine);
-            steps.push_back(crate::builder::Step::Pad);
-        } else if matches!(
-            child_expr.kind(),
-            rnix::SyntaxKind::NODE_ASSERT
-                | rnix::SyntaxKind::NODE_ATTR_SET
-                | rnix::SyntaxKind::NODE_PAREN
-                | rnix::SyntaxKind::NODE_LAMBDA
-                | rnix::SyntaxKind::NODE_LET_IN
-                | rnix::SyntaxKind::NODE_LIST
-                | rnix::SyntaxKind::NODE_STRING
-                | rnix::SyntaxKind::NODE_WITH
-        ) || (matches!(child_expr.kind(), rnix::SyntaxKind::NODE_APPLY)
+            // For expanded values, allow starting the value on the same line
+            // *if* it is a function. This allows constructs like
+            // foo = { some, arguments }:
+            //  lambda_body
+            if child_expr.kind() != rnix::SyntaxKind::NODE_LAMBDA {
+                steps.push_back(BuildStep::Indent);
+                steps.push_back(BuildStep::NewLine);
+                steps.push_back(BuildStep::Pad);
+            }
+            steps.push_back(BuildStep::Whitespace);
+        } else if {
+            use rnix::SyntaxKind::*;
+                matches!(
+                child_expr.kind(),
+                NODE_ASSERT
+                    | NODE_ATTR_SET
+                    | NODE_PAREN
+                    | NODE_LAMBDA
+                    | NODE_LET_IN
+                    | NODE_LIST
+                    | NODE_STRING
+                    | NODE_WITH
+        )} || (matches!(child_expr.kind(), rnix::SyntaxKind::NODE_APPLY)
             && crate::utils::second_through_penultimate_line_are_indented(
                 build_ctx,
                 child_expr.clone(),
                 false,
             ))
         {
-            steps.push_back(crate::builder::Step::Whitespace);
+            steps.push_back(BuildStep::Whitespace);
         } else {
             dedent = true;
-            steps.push_back(crate::builder::Step::Indent);
-            steps.push_back(crate::builder::Step::NewLine);
-            steps.push_back(crate::builder::Step::Pad);
+            steps.push_back(BuildStep::Indent);
+            steps.push_back(BuildStep::NewLine);
+            steps.push_back(BuildStep::Pad);
         }
     } else {
-        steps.push_back(crate::builder::Step::Whitespace);
+        steps.push_back(BuildStep::Whitespace);
     }
 
     // /**/
     for comment in comments_before {
         steps.push_back(comment);
-        steps.push_back(crate::builder::Step::NewLine);
-        steps.push_back(crate::builder::Step::Pad);
+        steps.push_back(BuildStep::NewLine);
+        steps.push_back(BuildStep::Pad);
     }
 
     // expr
     if vertical {
-        steps.push_back(crate::builder::Step::FormatWider(child_expr));
+        steps.push_back(BuildStep::FormatWider(child_expr));
         if !comments_after.is_empty() {
-            steps.push_back(crate::builder::Step::NewLine);
-            steps.push_back(crate::builder::Step::Pad);
+            steps.push_back(BuildStep::NewLine);
+            steps.push_back(BuildStep::Pad);
         }
     } else {
-        steps.push_back(crate::builder::Step::Format(child_expr));
+        steps.push_back(BuildStep::Format(child_expr));
     }
 
     // /**/
     for comment in comments_after {
         steps.push_back(comment);
-        steps.push_back(crate::builder::Step::NewLine);
-        steps.push_back(crate::builder::Step::Pad);
+        steps.push_back(BuildStep::NewLine);
+        steps.push_back(BuildStep::Pad);
     }
 
     // ;
     let child = children.get_next().unwrap();
-    steps.push_back(crate::builder::Step::Format(child));
+    steps.push_back(BuildStep::Format(child));
     if dedent {
-        steps.push_back(crate::builder::Step::Dedent);
+        steps.push_back(BuildStep::Dedent);
     }
 
     steps
